57be97e2d8e98af0a3947f1c368eafad5a71270c
ccarlaa <carlacarlaclementino@gmail.com>
2024-02-29 15:36:02 -0300

    fisrt code

---
+++ bigger_or_smaller.rb
@@ -0,0 +1,13 @@
+puts "Bem vindo, qual seu nome?"
+name = gets
+puts "\n"
+
+puts "Olá " + name
+puts "\n"
+
+puts "Escolha um número entre 0 e 200"
+number = gets
+secret_number = 145
+
+puts "\n"
+puts number == secret_number.to_i
